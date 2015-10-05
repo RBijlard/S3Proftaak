6bed45ab7981560e88bbd501ec6074d99b52b304
Berry-PC <Berry-PC@DESKTOP-7PI8GKE>
2015-10-05 09:34:22 +0200

    basicgame extention toegevoegd, en methods met comments etc.

--- S3Proftaak/src/s3proftaak/S3Proftaak.java
+++ S3Proftaak/src/s3proftaak/S3Proftaak.java
@@ -1,16 +1,61 @@
 package s3proftaak;
 
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
 /**
  *
  * @author Roel
  */
-public class S3Proftaak {
+public class S3Proftaak extends BasicGame {
 
     /**
+     * constructor for the S3Proftaak class
+     * @param title instance title
+     */
+    public S3Proftaak(String title) {
+        super(title);
+    }
+
+    /**
+     * Initialise method, runs once on the startup of the game
+     * @param container the window for the game
+     * @throws SlickException 
+     */
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        
+    }
+
+    /**
+     * Update method, runs every frame (insert logic here)
+     * @param container the window for the game
+     * @param delta delta value, difference in milliseconds between each frame
+     * @throws SlickException 
+     */
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+        
+    }
+
+    /**
+     * Render method, draws every update on the screen (draw in here)
+     * @param container the window for the game
+     * @param g graphics of the screen
+     * @throws SlickException 
+     */
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        
+    }
+    
+    /**
+     * Main method, runs the game thread and defines the app properties
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
