95062acdd5d09171073fa31857485ecf9a9ebcac
Roel Bijlard <roel.101@live.nl>
2015-11-02 14:25:58 +0100

    Dialogs toegevoegd voor incorect Username en Password enz

--- S3Proftaak/src/s3proftaak/Visuals/Login.java
+++ S3Proftaak/src/s3proftaak/Visuals/Login.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 import s3proftaak.Account;
 import s3proftaak.DBConnect;
 import s3proftaak.Main;
@@ -23,33 +24,44 @@ import static s3proftaak.Main.changeScreen;
  * @author Stan
  */
 public class Login extends BasicScene {
-    
-    @FXML TextField tfUsername;
-    @FXML PasswordField tfPassword;
-    @FXML Button btnLogin;
-    @FXML Button btnRegister;
-    
-    public void btnLoginClick(Event e){
+
+    @FXML
+    TextField tfUsername;
+    @FXML
+    PasswordField tfPassword;
+    @FXML
+    Button btnLogin;
+    @FXML
+    Button btnRegister;
+
+    public void btnLoginClick(Event e) {
         try {
-            if (DBConnect.getInstance().doUserLogin(tfUsername.getText(), tfPassword.getText())){
-                Account a = DBConnect.getInstance().getAccount(tfUsername.getText());
+            if (!tfUsername.getText().isEmpty() && !tfPassword.getText().isEmpty()) {
+                if (DBConnect.getInstance().doUserLogin(tfUsername.getText(), tfPassword.getText())) {
+                    Account a = DBConnect.getInstance().getAccount(tfUsername.getText());
                     Main.setAccount(a);
-                changeScreen(Main.Screens.Menu);
+                    changeScreen(Main.Screens.Menu);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Username or Password incorrect", "Login Error", 0);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Please Enter a Username and Password", "Registration Error", 0);
             }
         } catch (SQLException ex) {
             Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void btnRegisterClick(Event e){
+
+    public void btnRegisterClick(Event e) {
         try {
-            if (!DBConnect.getInstance().hasAccount(tfUsername.getText())){
-                if (!tfUsername.getText().isEmpty() && !tfPassword.getText().isEmpty()){
+            if (!tfUsername.getText().isEmpty() && !tfPassword.getText().isEmpty()) {
+                if (!DBConnect.getInstance().hasAccount(tfUsername.getText())) {
                     Account a = new Account(tfUsername.getText(), tfPassword.getText(), null);
                     DBConnect.getInstance().insertAccount(a);
-                }else{
-                    Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, "Enter an username & password.");
+                    JOptionPane.showMessageDialog(null, "Account Registered!", "Registration", 1);
                 }
+            } else {
+                JOptionPane.showMessageDialog(null, "Please Enter a Username and Password", "Registration Error", 0);
             }
         } catch (SQLException ex) {
             Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
