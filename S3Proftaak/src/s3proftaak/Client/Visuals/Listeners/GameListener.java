9a03ad18ff35a96321af6aa22b80b68dd6e411a7
Berry-PC <Berry-PC@DESKTOP-7PI8GKE>
2015-12-07 16:05:20 +0100

    g

--- S3Proftaak/src/s3proftaak/Client/Visuals/Listeners/GameListener.java
+++ S3Proftaak/src/s3proftaak/Client/Visuals/Listeners/GameListener.java
@@ -31,14 +31,20 @@ public class GameListener extends BasicListener {
             if (!evt.getOldValue().toString().equals(ClientAdministration.getInstance().getAccount().getUsername())) {
                 switch (evt.getPropertyName()) {
                     case "Rect":
+                        int offset = 0;
                         if (game.getGameCharacters() != null) {
+                            for(s3proftaak.Client.GameObjects.Character c : game.getGameCharacters()){
+                                if(c.getName().equals(ClientAdministration.getInstance().getAccount().getUsername())){
+                                    offset = (int)c.getOffsetX();
+                                }
+                            }
                             for (s3proftaak.Client.GameObjects.Character c : game.getGameCharacters()) {
                                 
                                 if (c.getName().equals(evt.getOldValue().toString())) {
                                     
                                     Rectangle rect = (Rectangle) evt.getNewValue();
                                     
-                                    c.getRect().setX(rect.getX());
+                                    c.getRect().setX(rect.getX() - offset);
                                     c.getRect().setY(rect.getY());
                                     c.getRect().setHeight(rect.getHeight());
                                     
